85616421165eb8ae43ede6251ae5d30f235cdd32
Bharath Chandra <bharathchandraks@gmail.com>
2019-01-05 17:06:31 +1100

    Added playlist component

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,25 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service'
+import {Video} from './video'
+import {PlaylistComponent} from './playlist.component '
+import { hasConstructor } from 'angular2/src/facade/lang';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
+
 })
 
-export class AppComponent {}
+export class AppComponent {
+    mainHeading = Config.MAIN_HEADING;
+    videos: Array<Video>
+
+   constructor() {
+       this.videos = [
+           new Video(1, "The Best Theories About Marvel Villains COMPILATION", "KwFYqrn_rUA", "Just theories"),
+           new Video(2, "Gordon Ramsay most funny moments", "WD3eDqljMgA", "Funny Moments")
+       ]
+   }
+
+}
